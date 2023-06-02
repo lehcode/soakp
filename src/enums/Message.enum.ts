f591083a966f6e7b8964780e6b19924c06dbc3c9
Lehcode <53556648+lehcode@users.noreply.github.com>
2023-06-02 09:06:23 +0300

    feat: add JWT_NOT_SAVED to Message enum

--- src/enums/Message.enum.ts
+++ src/enums/Message.enum.ts
@@ -4,6 +4,7 @@ export enum Message {
   INTERNAL_SERVER_ERROR = 'Internal server error',
   INVALID_KEY_STORAGE = 'Invalid key storage',
   JWT_ADDED = 'JWT added',
+  JWT_NOT_SAVED = 'JWT not saved',
   LOADED_JWT_TOKEN = 'Loaded JWT token',
   NOT_AUTHORIZED_ERROR = 'Not authorized',
   NOT_FOUND = 'Not found',
