1c5643f2887981a8e16d7e8123da4ba7e13b0361
Lehcode <53556648+lehcode@users.noreply.github.com>
2023-06-03 03:53:44 +0300

    feat: Add SSL certificate generation for myca service
    This commit adds SSL certificate generation for the myca service. The
    Dockerfile now sets an environment variable for the SSL certificates
    directory and creates a volume for it. The openssl.conf file is copied
    to the SSL certificates directory and used to generate the private key,
    CA certificate, server certificate, and PEM file. Sed commands are used
    to replace placeholders in openssl.conf with variables. Additionally,
    the entrypoint.sh script now includes commented-out lines for changing
    file ownership and listing SSL certificates directory. Finally, the
    docker-compose.yml file is updated to include the new environment
    variable and volume. The myca service now uses the lehcode/ssl-myca
    image instead of lehcode/ssl-ca-certs. The commit message follows the
    conventional commit convention.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,12 @@ if [ "${DEBUG}" = "yes" ]; then
   env
 fi
 
-echo ${NODE_USER_PWD} | sudo -S chown -R node:node ./
-echo ${NODE_USER_PWD} | sudo -S cat /etc/sudoers
-echo ${NODE_USER_PWD} | sudo -S ls -al ${DATA_FILE_DIR}
-sudo ls -al ${WORKDIR}
+# echo ${NODE_USER_PWD} | sudo -S chown -R node:node ./
+# echo ${NODE_USER_PWD} | sudo -S chown -R node:node /etc/ssl/soakp
+# echo ${NODE_USER_PWD} | sudo -S cat /etc/sudoers
+# echo ${NODE_USER_PWD} | sudo -S ls -al ${DATA_FILE_DIR}
+# sudo ls -al ${WORKDIR}
+# ls -al /etc/ssl/soakp
 
 if [ ! -d "node_modules" ]; then yarn install; fi
 
