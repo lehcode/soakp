03475ced85d777367fa1f8ca4b40adedb7626ad5
Lehcode <53556648+lehcode@users.noreply.github.com>
2023-06-04 18:21:07 +0300

    fix: Generate SSL certificates using myca Docker image
    This commit generates SSL certificates using the myca Docker image.
    The Dockerfile in `docker/myca` has been updated to include a new build
    argument `host_docker_gid`, which specifies the GID of the "docker"
    group of the host machine. The entrypoint script has been modified to
    copy the generated certificates to the directory specified in the
    `SSL_CERT_DIR` environment variable. The `docker-compose.yml` file has
    also been updated to use the new `SSL_CERT_DIR` variable instead of
    `SSL_CERTS_DIR`.

---
+++ docker/myca/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+if [ "${DEBUG}" = "yes" ]; then set -ex && env; fi
+
+mkdir -p "${SSL_CERT_DIR}"
+cp -rf "$(pwd)/ca" "${SSL_CERT_DIR}"
+cp -rf "$(pwd)/cert" "${SSL_CERT_DIR}"
+
+if [ "${DEBUG}" = "yes" ]; then
+  ls -al "${SSL_CERT_DIR}"
+fi
+
+if [ "${DEBUG}" = "yes" ]; then
+sleep infinity
+else
+  "$@"
+fi
