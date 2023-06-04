234249c361bdebc2ae11a1df69a27f64b4422a7c
Antony Repin <53556648+lehcode@users.noreply.github.com>
2023-06-04 22:03:21 +0300

    Merge develop (#16)
    * feat: Add SSL certificate configuration, deployment options, and update documentation
    
    This commit adds information on SSL certificate configuration in the README file and updates the Certbot.md file with a corrected command to start the Certbot service. The index.md file has been updated with a new section on features, and the name of a file has been changed from UsageScenarios.md to Scenarios.md. Additionally, a new file has been added under the deployment folder, CompileJS.md, which provides instructions for compiling JavaScript files.
    
    Added instructions for configuring SSL certificates, including generating self-signed certificates for local development and using existing certificates for localhost. Also added documentation for deployment options, including hosting on cloud platforms or local machines, generating JavaScript files for deployment, and deploying to AWS Lambda for serverless deployment.
    
    * Fix merge conflict
    
    * fix: Generate SSL certificates using myca Docker image
    
    This commit generates SSL certificates using the myca Docker image.
    The Dockerfile in `docker/myca` has been updated to include a new build
    argument `host_docker_gid`, which specifies the GID of the "docker"
    group of the host machine. The entrypoint script has been modified to
    copy the generated certificates to the directory specified in the
    `SSL_CERT_DIR` environment variable. The `docker-compose.yml` file has
    also been updated to use the new `SSL_CERT_DIR` variable instead of
    `SSL_CERTS_DIR`.
    
    * chore: ignore docker/myca/generate in .dockerignore and .gitignore files
    
    * feat: Add pipefail option and simplify entrypoint.sh script
    
    The script now uses the pipefail option for better error handling and
    the code has been simplified. The "sleep infinity" command has been
    replaced with a one-liner for better readability.
    
    * feat(env): configure SSL certificate for live domain
    
    Update the SERVER_HOST in .env.dist to your live domain. Update
    WILDCARD_DOMAIN to ",*.yourlivedomain.com". Update SSL_KEY_FILE and
    SSL_CERT_FILE to the name of SSL certificate files generated by myca
    or certbot service. Update SSL_CERT_DIR to the directory containing
    the SSL certificate.
    
    * fix(docker/myca): simplify entrypoint.sh
    
    Remove redundant debug code and simplify script to improve readability.
    
    * build: update Dockerfile and entrypoint.sh for host user customization
    
    This commit updates the Dockerfile to allow customization of the host
    user's UID and username, in addition to their password. It also updates
    the entrypoint.sh script to log the contents of the SSL cert directory
    in debug mode.
    
    Specifically, this commit:
    - Adds two new build arguments, `host_user_uid` and `host_user_name`,
      for the host user's UID and username, respectively
    - Replaces the `useradd` command in the Dockerfile with a command that
      uses the new build arguments to create the host user
    - Updates the `sudoers` file in the Dockerfile to allow the host user to
      run certain commands without a password
    - Replaces references to the `node` user in the Dockerfile and
      entrypoint.sh with references to the new host user
    - Adds a conditional in the entrypoint.sh script to log the contents of

--- docker/myca/entrypoint.sh
+++ docker/myca/entrypoint.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env sh
 
-set -e
-if [ "${DEBUG}" = "yes" ]; then set -ex && env; fi
+set -eo pipefail
+
+if [ "${DEBUG}" = "yes" ]; then
+  set -x
+  env
+fi
 
 mkdir -p "${SSL_CERT_DIR}"
 cp -rf "$(pwd)/ca" "${SSL_CERT_DIR}"
 cp -rf "$(pwd)/cert" "${SSL_CERT_DIR}"
 
-if [ "${DEBUG}" = "yes" ]; then
-  ls -al "${SSL_CERT_DIR}"
-fi
-
-if [ "${DEBUG}" = "yes" ]; then
-sleep infinity
-else
-  "$@"
-fi
+if [ "${DEBUG}" = "yes" ]; then ls -al "${SSL_CERT_DIR}"; fi
+if [ "${DEBUG}" = "yes" ]; then sleep infinity; fi
