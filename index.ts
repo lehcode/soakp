d3e3df00bed7179833c0523946a097542dd1a439
Lehcode <53556648+lehcode@users.noreply.github.com>
2023-06-09 01:23:45 +0300

    refactor: Fix typings

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 /**
- * Author: Lehcode
+ * Author: Anton Repin <53556648+lehcode@users.noreply.github.com>
  * Copyright: (C)2023.
  */
 import dotenv from 'dotenv';
-import { KeyStorage } from './src/KeyStorage';
-import { SoakpServer } from './src/SoakpServer';
+import KeyStorage, { StorageType } from './src/KeyStorage';
+import SoakpServer from './src/SoakpServer';
 
 dotenv.config();
 
--- src/backends/SQLite.ts
+++ src/backends/SQLite.ts
@@ -1,11 +1,7 @@
-/**
- * Author: Lehcode
- * Copyright: (C)2023
- */
 import { StatusCode } from '../enums/StatusCode.enum';
 import { Database } from 'sqlite3';
+import { DbSchemaInterface } from '../interfaces/DbSchema.interface';
 import { promises as fs } from 'fs';
-import { DbSchemaInterface } from '../KeyStorage';
 import path from 'path';
 import { Message } from '../enums/Message.enum';
 
