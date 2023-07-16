43b1f88d06b46fc1df8412eae9c78f64f98a6dfa
Lehcode <lehcode@gmail.com>
2023-07-17 02:43:37 +0300

    feat(server): implement KeyStorage in SoakpServer
    This commit adds the `KeyStorage` class to the `SoakpServer` implementation.
    
    - Import `KeyStorage` from './src/KeyStorage'
    - Modify the `Promise.resolve` block to initialize `KeyStorage` with `storageConfig`
    - Pass the `storage` instance to the `SoakpServer` constructor when starting the server

--- server.ts
+++ server.ts
@@ -3,18 +3,21 @@
  * Copyright: (C)2023
  */
 import { SoakpServer } from './src/SoakpServer';
-import serverConfig from './src/configs';
+import { serverConfig, storageConfig } from './src/configs';
 import { Message } from './src/enums/Message.enum';
+import { KeyStorage } from './src/KeyStorage';
 
-Promise.resolve(new SoakpServer(serverConfig).start()).then(() => {
-  console.log(`Started Secure OpenAI Key Proxy with TLS on port ${serverConfig.sslPort}.
+Promise.resolve(KeyStorage.getInstance(storageConfig)).then((storage) => {
+  Promise.resolve(new SoakpServer(serverConfig, storage).start()).then(() => {
+    console.log(`Started Secure OpenAI Key Proxy with TLS on port ${serverConfig.sslPort}.
 Please provide support here: https://opencollective.com/soakp`);
-}, (error: any) => {
-  if (error instanceof Error) {
-    throw error;
-  } else if (typeof error === 'string') {
-    throw new Error(error);
-  } else {
-    throw new Error(Message.UNKNOWN_ERROR);
-  }
+  }, (error: any) => {
+    if (error instanceof Error) {
+      throw error;
+    } else if (typeof error === 'string') {
+      throw new Error(error);
+    } else {
+      throw new Error(Message.UNKNOWN_ERROR);
+    }
+  });
 });
