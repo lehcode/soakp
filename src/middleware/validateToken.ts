15130a84d364ac2c38400cb3a2bbb3eaadf9a179
Lehcode <lehcode@gmail.com>
2023-07-17 02:41:34 +0300

    fix(middleware): validateToken logic and error handling
    - Fix the logic in the validateToken middleware function.
    - Handle errors properly and return appropriate status codes and messages.
    - Refactor the code to improve readability and maintainability.
    
    # Changes:
    - Renamed the parameter `key` to `jwtHash` for clarity.
    - Updated the status codes to use the StatusCode enum.
    - Added proper error handling and logging.
    - Refactored the code to handle token validation and storage more efficiently.

--- src/middleware/validateToken.ts
+++ src/middleware/validateToken.ts
@@ -2,30 +2,42 @@ import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { Message } from '../enums/Message.enum';
 import { KeyStorage } from '../KeyStorage';
+import { StatusCode } from '../enums/StatusCode.enum';
 
-const validateToken = (key: string, storage: KeyStorage) => {
+const validateToken = (jwtHash: string, storage: KeyStorage) => {
   return (req: Request, res: Response, next: NextFunction) => {
     // Assuming the token is provided in the "Authorization" header as "Bearer <token>"
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
-      return res.status(401).json({ message: Message.INVALID_JWT });
+      return res.status(StatusCode.NOT_AUTHORIZED).json({ message: Message.INVALID_JWT });
     }
 
     try {
-      const decoded = jwt.verify(token, key);
-      storage.getRecentToken().then(token => {
-        if (token === decoded.token) {
-          return res.status(200).json({ message: Message.JWT_ACCEPTED });
-        } else {
-          storage.saveToken(decoded.token);
-        }
-      });
+      const verified = <jwt.Jwt>jwt.verify(token, jwtHash);
 
-      req.user = decoded;
-      next();
+      storage.getRecentToken().then( async(recentToken) => {
+        if (recentToken) {
+          if (token === recentToken) {
+            console.log('Found existing token');
+            req.user.token = recentToken;
+          }
+        } else {
+          const status = await storage.saveToken(token);
+          console.log('Accepted and saved new token');
+
+          if (status === StatusCode.CREATED) {
+            req.user.token = token;
+          } else {
+            return res.status(StatusCode.INTERNAL_ERROR).json({ message: Message.INTERNAL_SERVER_ERROR });
+          }
+        }
+
+        next();
+      });
     } catch (error) {
-      return res.status(401).json({ message: Message.INVALID_JWT });
+      console.log(error);
+      return res.status(StatusCode.INTERNAL_ERROR).json({ message: Message.INTERNAL_SERVER_ERROR });
     }
   };
 };
