1b1b85169b8368a952e34efc29cbf957cd8b7b34
Lehcode <lehcode@gmail.com>
2023-07-17 14:43:27 +0300

    feat(server): implement async server start
    - Refactor the server start logic to use async/await.
    - Add error handling for any potential errors during server start.

--- server.ts
+++ server.ts
@@ -7,11 +7,14 @@ import { serverConfig, storageConfig } from './src/configs';
 import { Message } from './src/enums/Message.enum';
 import { KeyStorage } from './src/KeyStorage';
 
-Promise.resolve(KeyStorage.getInstance(storageConfig)).then((storage) => {
-  Promise.resolve(new SoakpServer(serverConfig, storage).start()).then(() => {
+const startServer = async () => {
+  try {
+    const storage = await KeyStorage.getInstance(storageConfig);
+    const server = new SoakpServer(serverConfig, storage);
+    await server.start();
     console.log(`Started Secure OpenAI Key Proxy with TLS on port ${serverConfig.sslPort}.
 Please provide support here: https://opencollective.com/soakp`);
-  }, (error: any) => {
+  } catch (error) {
     if (error instanceof Error) {
       throw error;
     } else if (typeof error === 'string') {
@@ -19,5 +22,7 @@ Please provide support here: https://opencollective.com/soakp`);
     } else {
       throw new Error(Message.UNKNOWN_ERROR);
     }
-  });
-});
+  }
+};
+
+startServer();
