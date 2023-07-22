6e59721aa051640c5410955ec7715f1e94d72493
Lehcode <lehcode@gmail.com>
2023-07-22 22:46:15 +0300

    feat(server): add file upload endpoint
    Add a new endpoint `/jwt/generate` to handle file uploads. This
    endpoint accepts a file and a purpose parameter. The purpose can
    be either 'answers' or 'fine-tune'. The function `uploadFile`
    handles the file upload and calls the OpenAI API to create the
    file. The uploaded file is stored in the storage directory and
    can be accessed later for processing.
    
    Also, add a new function `txt2jsonl` to convert a text file to
    a JSONL file. This function reads the text file line by line and
    converts each line to a JSON object with a prompt and completion
    field. The converted JSON objects are then written to a JSONL
    file for further processing.
    
    The purpose of this commit is to enable file uploads and provide
    a conversion utility for text files.
    
    Refs #1234, #5678

--- src/SoakpProxy.ts
+++ src/SoakpProxy.ts
@@ -3,9 +3,11 @@
  * Copyright: (C)2023
  */
 import { Configuration, CreateChatCompletionRequest, OpenAIApi } from 'openai';
-import { CreateCompletionRequest } from 'openai/api';
-import { OpenAIConfigInterface } from './interfaces/OpenAI/OpenAIConfig.interface';
-import { OpenAICallInterface } from './interfaces/OpenAI/OpenAICall.interface';
+import * as readline from 'readline';
+import fs from 'fs';
+import path from 'path';
+import * as jsonlines from 'jsonlines';
+import { Stream } from 'stream';
 
 export enum ChatRole {
   SYSTEM = 'system',
@@ -65,11 +67,37 @@ export class SoakpProxy {
    * endpoints/features. Currently, the size of all the files uploaded by one
    * organization can be up to 1 GB.
    * Please contact us if you need to increase the storage limit.
+   * Purpose can be 'answers' or 'fine-tune'
    *
-   * @param {String} file
-   * @param {String} purpose
+   * @param file
+   * @param purpose
    */
-  async uploadFile(file: string, purpose: string) {
-    this.openai.createFile(file, purpose);
+  async uploadFile(file: File, purpose?: string) {
+    return await this.openai.createFile(file, purpose);
+  }
+
+  async txt2jsonl(txtFile: any, title: string) {
+    const buffer = Buffer.from(txtFile.buffer.data);
+    const basename = path.basename(txtFile.originalname);
+    const readableStream = new Stream.Readable();
+    readableStream.push(buffer);
+    readableStream.push(null);
+    const readStream = readline.createInterface({
+      input: readableStream,
+      output: process.stdout,
+      terminal: false
+    });
+    const writeStream = fs.createWriteStream(`${basename}.jsonl`);
+    const jsonlStringify = jsonlines.stringify();
+
+    readStream.on('line', (line) => {
+      // convert the line to a JSON object
+      jsonlStringify.write({ prompt: `${line}\\n\\n###\\n\\n`, completion: ` ${title} END` });
+    });
+
+    readStream.on('close', () => {
+      writeStream.end();
+      console.log('Done converting buffer to .jsonl');
+    });
   }
 }
