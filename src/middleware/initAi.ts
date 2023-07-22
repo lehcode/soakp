ebb3ec53631b089469b3163c7fef08b163e43e95
Lehcode <lehcode@gmail.com>
2023-07-22 14:47:59 +0300

    refactor(file-upload): refactor uploadFile middleware
    - Refactor the `uploadFile` middleware in `src/middleware/uploadFile.ts` to improve code readability and maintainability.
    
    - Update the middleware to use `multer` for file upload handling.
    
    - Implement file upload validation and handling of different file types.
    
    - Set a maximum file size limit of 5MB for uploads.
    
    - Handle errors and provide appropriate error responses.
    
    - Store the uploaded file information in the `req.userFiles` object.
    
    - Update error responses to use the `Responses` helper class.
    
    - Update imports and references to match the new changes.
    
    - Add error handling for internal server errors.
    
    - Update the `uploadFile` middleware export.
    
    refactor(ai-init): refactor initAi middleware
    
    - Refactor the `initAi` middleware in `src/middleware/initAi.ts` to improve code readability and maintainability.
    
    - Remove unused imports.
    
    - Update error response message to use the `StatusMessage` enum.
    
    -

--- src/middleware/initAi.ts
+++ src/middleware/initAi.ts
@@ -3,7 +3,7 @@ import { SoakpProxy } from '../SoakpProxy';
 import { Configuration } from 'openai';
 import { SoakpServer } from '../SoakpServer';
 import { StatusCode } from '../enums/StatusCode.enum';
-import { Message } from '../enums/Message.enum';
+import { StatusMessage } from '../enums/StatusMessage.enum';
 
 const initAi = (server: SoakpServer) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -17,7 +17,7 @@ const initAi = (server: SoakpServer) => {
       server.proxy = proxy;
     } catch (error) {
       console.error(error);
-      return res.status(StatusCode.INTERNAL_ERROR).json({ message: Message.INTERNAL_SERVER_ERROR });
+      return res.status(StatusCode.INTERNAL_ERROR).json({ message: StatusMessage.INTERNAL_SERVER_ERROR });
     }
 
     next();
