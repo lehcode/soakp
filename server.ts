1dc739dca3856d282dd6e8a5984a5ecf9c7e8754
Lehcode <lehcode@gmail.com>
2023-07-23 10:40:33 +0300

    feat(server): Add support for generating new JWT
    - Added support for generating a new JWT if the OpenAI API key is undefined.
    - Displayed the command to generate a new JWT in the console.

--- server.ts
+++ server.ts
@@ -10,17 +10,18 @@ import { KeyStorage } from './src/KeyStorage';
 const startServer = async () => {
   try {
     const storage = await KeyStorage.getInstance(storageConfig);
-
-    if (serverConfig.openAI.apiKey === undefined) {
-      console.log(`Existing valid JWT not found. You can generate new one using:
-curl -l 'https://localhost:3033/jwt/generate' --header 'Content-Type: application/json' --header 'Authorization: Basic <basic_auth_token>' --data '{ "key": "<sk-apikey>", "orgId": "<org-orgId>"`);
-    }
-
     const server = new SoakpServer(serverConfig, storage);
     await server.start();
 
     console.log(`Started Secure OpenAI Key Proxy with TLS on port ${serverConfig.sslPort}.
 Please provide support here: https://opencollective.com/soakp`);
+    if (serverConfig.openAI.apiKey === undefined) {
+      console.log('Existing valid JWT not found. You can generate new one using:\n' +
+        'curl --location-trusted \'https://localhost:3033/jwt/generate\'' + ' \\\n\t' +
+        '--header \'Content-Type: application/json\'' + ' \\\n\t' +
+        '--header \'Authorization: Basic <basic_auth_token>\'' + ' \\\n\t' +
+        '--data \'{ "key": "<sk-apikey>", "orgId": "<org-orgId>" }\'');
+    }
   } catch (error) {
     if (error instanceof Error) {
       throw error;
